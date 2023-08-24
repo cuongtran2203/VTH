29d0d7fc6373c13158a0f58f5375577137e57c4e
Trần Đình Mạnh Cường <81034438+cuongtran2203@users.noreply.github.com>
2023-08-24 09:57:38 +0700

    .

--- get_10s.py
+++ get_10s.py
@@ -19,12 +19,16 @@ df["My"]=df["ranges"]*(np.cos(df["elevations"])*np.cos(df["azimuths"]))
 df["Mz"]=df["ranges"]*(np.sin(df["elevations"]))
 # df["sleep"]=np.int64(df["timestamps"])-int(df["timestamps"][0])
 df=df.sort_values(by=["timestamps"])
-fig,ax=plt.subplots()
+# fig,ax=plt.subplots()
+fig = plt.figure(figsize=(7,7))
 ax = fig.add_subplot(111, projection='3d')
 track_org=df["timestamps"][0]
 time_track=False
 df_truncate_10s=None
 df_new=None
+ax.set_xlim(df["Mx"].min(),df["Mx"].max())
+ax.set_ylim(df["My"].min(),df["My"].max())
+ax.set_zlim(df["Mz"].min(),df["Mz"].max())
 def animate(i):
     global track_org,df_truncate_10s,df_new,count
     if df_truncate_10s is None and df_new is None:
@@ -33,19 +37,12 @@ def animate(i):
     else:
         df_new=df_new[~df_new.isin(df_truncate_10s)]
         df_truncate_10s=df_new.loc[df_new["timestamps"]-track_org<=10000]
-       
-        # df_truncate_10s=df.loc[df_truncate_10s["timestamps"].iloc[-1]]
-    # print(df_truncate_10s)
-    print(df_truncate_10s)
     Mx_vi=df_truncate_10s["Mx"]
     My_vi=df_truncate_10s["My"]
     Mz_vi=df_truncate_10s["Mz"]
-    ax.set_xlim(df["Mx"].min(),df["Mx"].max())
-    ax.set_ylim(df["My"].min(),df["My"].max())
-    ax.set_zlim(df["Mz"].min(),df["Mz"].max())
     ax.clear()
-    ax.scatter(Mx_vi,My_vi,Mz_vi)
-    # ax.set_label("change pharse from {} to {}".format(track_org,df_truncate_10s["timestamps"].iloc[-1]))
+    # s=Mz_vi/((My_vi*0.01)**2)
+    ax.scatter(Mx_vi,My_vi,Mz_vi,s=20,color="red")
     track_org=df_truncate_10s["timestamps"].iloc[-1]
     print(track_org)
     print("change pharse")
@@ -53,7 +50,7 @@ def animate(i):
 ani = animation.FuncAnimation(
     fig, animate,frames=800, interval=100,repeat=False
 )
-ani.save("results.gif")
+# ani.save("results.gif")
 plt.show() 
 
 
