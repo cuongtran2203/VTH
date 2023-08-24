51339b8ac4b77be02ba044a2d764bdc364633f9e
Trần Đình Mạnh Cường <81034438+cuongtran2203@users.noreply.github.com>
2023-08-24 09:23:11 +0700

    .

--- get_10s.py
+++ get_10s.py
@@ -17,38 +17,43 @@ Với e là elevation ,r là range , az là azimuths
 df["Mx"]=df["ranges"]*(np.cos(df["elevations"])*np.sin(df["azimuths"]))
 df["My"]=df["ranges"]*(np.cos(df["elevations"])*np.cos(df["azimuths"]))
 df["Mz"]=df["ranges"]*(np.sin(df["elevations"]))
-df["sleep"]=np.int64(df["timestamps"])-int(df["timestamps"][0])
-df=df.sort_values(by=["timestamps"],ascending=False)
+# df["sleep"]=np.int64(df["timestamps"])-int(df["timestamps"][0])
+df=df.sort_values(by=["timestamps"])
 fig,ax=plt.subplots()
 ax = fig.add_subplot(111, projection='3d')
 track_org=df["timestamps"][0]
-time_track=df["timestamps"][0]
+time_track=False
+df_truncate_10s=None
+df_new=None
 def animate(i):
-    global time_track,track_org
-    
-    print("df",df["timestamps"][i] )
-    #theo dõi timestamps theo từng điểm
-    time_track=df["timestamps"][i]
-    '''
-    lấy ra toàn bộ các điểm trong 1 chu kì 10s
-    '''
-    Mx_vi=df[df["timestamps"]-track_org<10100]["Mx"]
-    My_vi=df[df["timestamps"]-track_org<10100]["My"]
-    Mz_vi=df[df["timestamps"]-track_org<10100]["Mz"]
-    ax.scatter(Mx_vi,My_vi,Mz_vi)
+    global track_org,df_truncate_10s,df_new,count
+    if df_truncate_10s is None and df_new is None:
+        df_truncate_10s=df.loc[df["timestamps"]-track_org<=10000]
+        df_new=df[~df.isin(df_truncate_10s)]
+    else:
+        df_new=df_new[~df_new.isin(df_truncate_10s)]
+        df_truncate_10s=df_new.loc[df_new["timestamps"]-track_org<=10000]
+       
+        # df_truncate_10s=df.loc[df_truncate_10s["timestamps"].iloc[-1]]
+    # print(df_truncate_10s)
+    print(df_truncate_10s)
+    Mx_vi=df_truncate_10s["Mx"]
+    My_vi=df_truncate_10s["My"]
+    Mz_vi=df_truncate_10s["Mz"]
     ax.set_xlim(df["Mx"].min(),df["Mx"].max())
     ax.set_ylim(df["My"].min(),df["My"].max())
     ax.set_zlim(df["Mz"].min(),df["Mz"].max())
-    if time_track-track_org>10100:
-        ax.clear()
-        track_org=time_track
-        print("change pharse")
-        print("time org",track_org)
-        print("time tracking",time_track)
+    ax.clear()
+    ax.scatter(Mx_vi,My_vi,Mz_vi)
+    # ax.set_label("change pharse from {} to {}".format(track_org,df_truncate_10s["timestamps"].iloc[-1]))
+    track_org=df_truncate_10s["timestamps"].iloc[-1]
+    print(track_org)
+    print("change pharse")
+    print("time org",track_org)
 ani = animation.FuncAnimation(
-    fig, animate,frames=df["Mx"].unique().size, interval=5
+    fig, animate,frames=800, interval=100,repeat=False
 )
 ani.save("results.gif")
-# plt.show() 
+plt.show() 
 
 
